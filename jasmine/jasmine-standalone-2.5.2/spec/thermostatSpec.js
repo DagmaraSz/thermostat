3ce7494890c227b48bf7142c40572396c095dd9d
Dagmara <daga.sz@hotmail.com>
2016-10-26 12:11:18 +0100

    More stories

--- jasmine/jasmine-standalone-2.5.2/spec/thermostatSpec.js
+++ jasmine/jasmine-standalone-2.5.2/spec/thermostatSpec.js
@@ -12,19 +12,38 @@ describe ("Thermostat", function(){
   });
 
   it("can increase temperature", function(){
-    var initial_temp = thermostat.temp();
-    thermostat.increase_temp();
-    expect(thermostat.temp()).toEqual(initial_temp + 1);
+    var initialTemp = thermostat.temp();
+    thermostat.increaseTemp();
+    expect(thermostat.temp()).toEqual(initialTemp + 1);
   });
 
   it("can decrease temperature", function(){
-    var initial_temp = thermostat.temp();
-    thermostat.decrease_temp();
-    expect(thermostat.temp()).toEqual(initial_temp - 1);
+    var initialTemp = thermostat.temp();
+    thermostat.decreaseTemp();
+    expect(thermostat.temp()).toEqual(initialTemp - 1);
   });
 
   it("has a minimum temperature of 10", function(){
-    _(10).times(function(){ thermostat.decrease_temp(); });
-    expect(function(){thermostat.decrease_temp();}).toThrow("Thermostat has reached minimum temperature");
+    _(10).times(function(){ thermostat.decreaseTemp(); });
+    expect(function(){thermostat.decreaseTemp();}).toThrow("Thermostat has reached minimum temperature");
   });
+
+  describe("power saving mode", function(){
+
+    it("is on by default",function(){
+      expect(thermostat.isPowerSaving()).toEqual(true);
+    });
+
+    it("has max temp. of 25 if mode is on", function(){
+      _(5).times(function(){ thermostat.increaseTemp(); });
+      expect(function(){thermostat.increaseTemp();}).toThrow("Thermostat has reached maximum temperature (power saving on)");
+    });
+
+    it("has max temp. of 32 if mode is off", function(){
+      thermostat.changePowerSaving();
+      _(12).times(function(){ thermostat.increaseTemp(); });
+      expect(function(){thermostat.increaseTemp();}).toThrow("Thermostat has reached maximum temperature (power saving off)");
+    });
+  });
+
 });
--- jasmine/jasmine-standalone-2.5.2/src/thermostat.js
+++ jasmine/jasmine-standalone-2.5.2/src/thermostat.js
@@ -2,16 +2,37 @@
 
 function Thermostat(){
   this.temperature = 20;
+  this.savingMode = true;
 };
 
+// readers
 Thermostat.prototype.temp = function () {
   return this.temperature;
 };
-Thermostat.prototype.increase_temp = function () {
+Thermostat.prototype.isPowerSaving = function () {
+  return this.savingMode;
+};
+
+//functions
+Thermostat.prototype.increaseTemp = function () {
+  this.maxTemp();
   this.temperature += 1;
 };
 
-Thermostat.prototype.decrease_temp = function () {
-  if (this.temperature === 10) throw "Thermostat has reached minimum temperature";
+Thermostat.prototype.decreaseTemp = function () {
+  this.minimumTemp();
   this.temperature -= 1;
+};
+
+Thermostat.prototype.changePowerSaving = function() {
+  this.isPowerSaving ? (this.savingMode = false) : (this.savingMode = true);
+};
+
+Thermostat.prototype.minimumTemp = function () {
+  if (this.temp() === 10) throw "Thermostat has reached minimum temperature";
+};
+
+Thermostat.prototype.maxTemp = function () {
+  if (this.isPowerSaving() && this.temp() === 25) throw "Thermostat has reached maximum temperature (power saving on)";
+  if (this.isPowerSaving() === false && this.temp() === 32) throw "Thermostat has reached maximum temperature (power saving off)";
 };
